696b8b9c771a2c128ef55f273e623fda85ce2648
Artur S <inaxit.artur@gmail.com>
2026-02-07 11:24:09 +0100

    Commit #2

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,12 +1,7 @@
 # Imports
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn import linear_model, metrics, svm, ensemble
+from sklearn import linear_model, metrics, svm, ensemble, preprocessing
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -95,21 +90,23 @@ y = df["passed exam"].to_numpy()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12345, stratify=y,)
 
-# universal method for training and evaluation of models
+encoder = preprocessing.LabelEncoder()
+y_train = encoder.fit_transform(y_train)
+y_test = encoder.fit_transform(y_test)
 
-def train_model(classifier, feature_vector_train, label, feature_vector_valid):
+# universal method for model training
+
+def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False):
     # train model
     classifier.fit(feature_vector_train, label)
-    # universal method for training and evaluation of models
-
-def train_model(classifier, feature_vector_train, label, feature_vector_valid):
-    # train model
-    classifier.fit(feature_vector_train, label)
-
-    # generate labels for validation set
+    
+    # generate predictions for test set
     predictions = classifier.predict(feature_vector_valid)
-
-    # evealuate model based on test set labels
+    
+    if is_neural_net:
+        predictions = predictions.argmax(axis=-1)
+    
+    # evaluate model
     scores = list(metrics.precision_recall_fscore_support(predictions, y_test))
     score_vals = [
         scores[0][0],
@@ -117,64 +114,33 @@ def train_model(classifier, feature_vector_train, label, feature_vector_valid):
         scores[2][0]
     ]
     score_vals.append(metrics.accuracy_score(predictions, y_test))
-    return score_vals
-    # generate labels for validation set
-    predictions = classifier.predict(feature_vector_valid)
+    return classifier, score_vals
 
-    # evealuate model based on test set labels
-    scores = list(metrics.precision_recall_fscore_support(predictions, y_test))
-    score_vals = [
-        scores[0][0],
-        scores[1][0],
-        scores[2][0]
-    ]
-    score_vals.append(metrics.accuracy_score(predictions, y_test))
-    return score_vals
+# MODEL 1 - logistic regression
+lr_model, accuracy = train_model(linear_model.LogisticRegression(), X_train, y_train, X_test)
+accuracy_compare = {'LR': accuracy}
+#print ("LR, accuracy: ", accuracy)
 
+# MODEL 2 - Support Vector Machine
+svm_model, accuracy = train_model(svm.SVC(probability=True), X_train, y_train, X_test)
+accuracy_compare['SVM'] = accuracy
+#print ("SVM, accuracy: ", accuracy)
 
-unique, counts = np.unique(y, return_counts=True)
-#print(dict(zip(unique, counts)))
-
-
-
-# Logistic Regression pipeline
-lr_pipeline = Pipeline([
-    ("scaler", StandardScaler()),
-    ("lr", LogisticRegression(max_iter=1000))
-])
-lr_pipeline.fit(X_train, y_train)
-accuracy_lr = accuracy_score(y_test, lr_pipeline.predict(X_test))
-#print("LR Accuracy:", accuracy_lr)
-
-# SVM pipeline
-svm_pipeline = Pipeline([
-    ("scaler", StandardScaler()),
-    ("svm", svm.SVC(class_weight="balanced", probability=True)) 
-])
-svm_pipeline.fit(X_train, y_train)
-accuracy_svm = accuracy_score(y_test, svm_pipeline.predict(X_test))
-#print("SVM Accuracy:", accuracy_svm)
-
-rf = RandomForestClassifier(
-    n_estimators=200, 
-    random_state=13579,
-    class_weight="balanced"
-)
-rf.fit(X_train, y_train)
-accuracy_rf = accuracy_score(y_train, y_train)
-#print("Random Forest Test Accuracy:", accuracy_rf)
-df["passed exam"].value_counts(normalize=True)
+# MODEL 3 - Random Forest Tree 
+rft_model, accuracy = train_model(ensemble.RandomForestClassifier(), X_train, y_train, X_test)
+accuracy_compare['RF'] = accuracy
+#print ("RF, accuracy: ", accuracy)
 
 
 import streamlit as st
 
 # ui
-st.title("Predykcja zdania egzaminu")
+st.title("Prediction of passing an exams: math, reading, writing.")
 
-gender = st.selectbox("Płeć", ["male", "female"])
-course = st.selectbox("Ukończony kurs przygotowawczy?", ["True", "False"])
-lunch = st.selectbox("Zjedzony lunch?", ["True", "False"])
-education = st.selectbox("Ukończona edukacja", [
+gender = st.selectbox("Gender", ["male", "female"])
+course = st.selectbox("Completed course?", ["Yes", "No"])
+lunch = st.selectbox("Ate lunch?", ["Yes", "No"])
+education = st.selectbox("Completed education", [
     "associate's degree", "bachelor's degree", "high school", "master's degree",
     "some college", "some high school"
 ])
@@ -184,17 +150,17 @@ new_data = pd.DataFrame([{col:0 for col in categories}])
 
 # Ustawienie odpowiednich kolumn na 1
 new_data[gender] = 1
-new_data["had lunch"] = 1 if lunch=="True" else 0
-new_data["completed preparation course"] = 1 if course=="True" else 0
+new_data["had lunch"] = 1 if lunch=="Yes" else 0
+new_data["completed preparation course"] = 1 if course=="Yes" else 0
 new_data[education] = 1
 
 # prawdopodobienstwo
-prob_lr = lr_pipeline.predict_proba(new_data)[:,1][0]
-prob_svm = svm_pipeline.predict_proba(new_data)[:,1][0]
-prob_rf = rf.predict_proba(new_data)[:,1][0]
+prob_lr = lr_model.predict_proba(new_data)[:,1][0]
+prob_svm = svm_model.predict_proba(new_data)[:,1][0]
+prob_rf = rft_model.predict_proba(new_data)[:,1][0]
 
 # Wyświetlenie wyników
-st.subheader("Prawdopodobieństwo zdania egzaminu:")
+st.subheader("Probability of passing an exams:")
 st.write(f"Logistic Regression: {round(prob_lr*100, 2)}%")
-st.write(f"SVM: {round(prob_svm*100, 2)}%")
+st.write(f"Support Vector Machine: {round(prob_svm*100, 2)}%")
 st.write(f"Random Forest: {round(prob_rf*100, 2)}%")
